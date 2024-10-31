8bf7d8472a7eb5ee7f455b0d28d88f1e00d6f215
May <abeillee@bk.ru>
2024-10-31 21:22:46 +0300

    feat: add solution for findLongestWord

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -362,8 +362,9 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(entence) {
+  const arr = entence.split(' ').sort((a, b) => b.length - a.length);
+  return arr[0];
 }
 
 /**
